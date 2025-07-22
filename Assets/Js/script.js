0628c0f3cf4282879744aef4680a4027764f327c
Flamingo <ernestyombi20@gmail.com>
2025-07-22 13:50:42 +0200

    mise a jour

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -5,10 +5,26 @@ const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
 
+// Boutons pour desktop
 registerBtn?.addEventListener('click', () => {
   container.classList.add("active");
 });
 
 loginBtn?.addEventListener('click', () => {
   container.classList.remove("active");
+});
+
+// --- Gestion du bouton mobile
+const mobileSwitchBtn = document.getElementById('mobile-switch-btn');
+const switchText = document.getElementById('switch-text');
+
+mobileSwitchBtn?.addEventListener('click', () => {
+  container.classList.toggle("active");
+  
+  // Met à jour le texte du bouton
+  if (container.classList.contains("active")) {
+    switchText.textContent = "Se connecter";
+  } else {
+    switchText.textContent = "Créer un compte";
+  }
 });
